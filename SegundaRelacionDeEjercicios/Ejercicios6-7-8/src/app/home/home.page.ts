bf9478a7c6c5105389c2b01d08dec7670d121302
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2019-11-06 11:01:20 +0100

    Ejercicios hechos

--- SegundaRelacionDeEjercicios/Ejercicios6-7-8/src/app/home/home.page.ts
+++ SegundaRelacionDeEjercicios/Ejercicios6-7-8/src/app/home/home.page.ts
@@ -10,11 +10,19 @@ import { Film } from '../model/film';
 export class HomePage {
 
   films: Film[];
+  clicked: boolean;
+  index: number;
 
   constructor(private service: FilmService) {
   }
 
   ionViewWillEnter() {
     this.films = this.service.getFilms();
+    this.films.forEach(f => f.cover = "assets/img/" + f.cover);
+  }
+
+  showDetails(title: string) {
+    this.index = this.films.findIndex(f => f.title == title);
+    this.clicked = !this.clicked;
   }
 }
